690171431cc15175a0c18ef9a5f45db065faef04
Andrew Thigpen <andyt05@gmail.com>
2017-08-13 15:09:40 -0500

    wip| fixup detect.

--- custom_components/myvoice/README.md
+++ custom_components/myvoice/README.md
@@ -3,3 +3,4 @@
 * Install manually via the github repo (no binary distributions for ARM)
 * Requires Swig 3.0.12+, also installed manually
 * May require modifying setup.py to use 'Python3' instead of 'Python'
+* Modify snowboydecoder to add the correct `input_device_index` for `self.audio.open()`
--- custom_components/myvoice/detect.py
+++ custom_components/myvoice/detect.py
@@ -1,43 +1,47 @@
+"""
+Hotword detector
+"""
 import sys
 import snowboy
 
-# fix path for snowboy
+from homeassistant.const import EVENT_HOMEASSISTANT_STOP
+
+# fix import path for snowboy
 sys.path.extend(snowboy.__path__._path)
 
 import snowboydecoder
-import signal
 
-interrupted = False
-
-
-def signal_handler(signal, frame):
-    global interrupted
-    interrupted = True
+DOMAIN = 'myvoice'
+CONF_MODEL = 'model'
+CONF_SENSITIVITY = 'sensitivity'
+EVENT_DETECTED = 'detected'
 
 
-def interrupt_callback():
-    global interrupted
-    return interrupted
+def setup(hass, config):
+    """Sets up the hotword detector"""
+    model = config[DOMAIN].get(CONF_MODEL, 'resources/alexa.umdl')
+    sensitivity = config[DOMAIN].get(CONF_SENSITIVITY, [])
+    audio_gain = config[DOMAIN].get(CONF_AUDIO_GAIN, 1)
 
-def detected():
-    print("detected")
+    detector = snowboydecoder.HotwordDetector(
+        model, sensitivity=sensitivity, audio_gain=audio_gain)
+    shutting_down = False
 
-if len(sys.argv) == 1:
-    print("Error: need to specify model name")
-    print("Usage: python demo.py your.model")
-    sys.exit(-1)
+    def detected():
+        """Fires an event when the keyword is detected"""
+        hass.bus.fire('{}.{}'.format(DOMAIN, EVENT_DETECTED))
 
-model = sys.argv[1]
+    def stop_detector(event):
+        """Stops the detector instance on shutdown"""
+        shutting_down = True
+        detector.terminate()
 
-# capture SIGINT signal, e.g., Ctrl+C
-signal.signal(signal.SIGINT, signal_handler)
+    def interrupt_callback():
+        """Called periodically by the detector.  Return True to stop."""
+        return shutting_down
 
-detector = snowboydecoder.HotwordDetector(model) #, sensitivity=0.8)
-print('Listening... Press Ctrl+C to exit')
+    detector.start(detected_callback=detected,
+                   interrupt_check=interrupt_callback,
+                   sleep_time=0.03)
 
-# main loop
-detector.start(detected_callback=detected,
-               interrupt_check=interrupt_callback,
-               sleep_time=0.03)
-
-detector.terminate()
+    hass.bus.listen_once(EVENT_HOMEASSISTANT_STOP, stop_detector)
