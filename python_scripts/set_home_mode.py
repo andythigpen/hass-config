7a3df00f47726c41a60da3c6716e3daae1ecd226
Andrew Thigpen <andyt05@gmail.com>
2022-08-06 18:48:11 -0500

    Remove set_home_mode, fix issues.

--- python_scripts/set_home_mode.py
+++
@@ -1,8 +0,0 @@
-"""
-Wrapper for setting the current home 'mode'
-"""
-
-hass.services.call('input_select', 'select_option', {
-    'entity_id': 'input_select.myhome_mode',
-    'option': data.get('mode').title(),
-})
